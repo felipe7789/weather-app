489d7023606a96bc926496014a13df2048a19ddf
felipe7789 <oshirokfelipe@gmail.com>
2024-09-29 10:56:56 -0300

    feat(t1): obtem previsao a partir de coords

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const { API_KEY }=process.env
 var cidade = 'Sao Paulo'
 var lat
 var long
+var temp
 
 let takeCordenate = (cidade) => {
     let url = `http://api.openweathermap.org/geo/1.0/direct?q=${cidade}&limit=1&appid=${API_KEY}`
@@ -17,3 +18,23 @@ let takeCordenate = (cidade) => {
         })
 }
 
+
+let takeSensation = (lat, long) => {
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${API_KEY}`
+    return fetch(url)
+        .then(response => response.json())
+        .then(data =>{
+            temp=data.main.feels_like
+            return{temp}
+        })
+}
+
+takeCordenate('São Paulo')
+    .then(({ lat, long }) => {
+        console.log('Latitude = '+ lat +'\nLongitude = ' + long)
+        return takeSensation(lat, long)
+    })
+    .then(({temp}) => {
+        console.log('Sensasao termica = '+ temp)
+    })
+    .catch(error => console.error('Erro:', error))
